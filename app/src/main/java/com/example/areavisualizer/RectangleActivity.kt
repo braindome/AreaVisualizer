fa434b7261e5627423dd26cfd413cd6f8c6cacc8
Antonio Saracino <antonio.saracino83@gmail.com>
2022-11-13 01:58:24 +0100

    Added rectangle and triangle.

--- app/src/main/java/com/example/areavisualizer/RectangleActivity.kt
+++ app/src/main/java/com/example/areavisualizer/RectangleActivity.kt
@@ -1,11 +1,37 @@
 package com.example.areavisualizer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class RectangleActivity : AppCompatActivity() {
+
+    lateinit var heightEditText : EditText
+    lateinit var lengthEditText : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rectangle)
+
+        val rectBtn : Button = findViewById<Button>(R.id.rectCalcBtnView)
+
+        heightEditText = findViewById(R.id.heightEditText)
+        lengthEditText = findViewById(R.id.lengthEditText)
+
+        rectBtn.setOnClickListener() {
+            calculateArea()
+            val intent = Intent(this, ResultActivity::class.java )
+            intent.putExtra("calculateArea", calculateArea())
+            startActivity(intent)
+
+        }
+    }
+
+    fun calculateArea() : Int {
+        var length = lengthEditText.text.toString().toInt()
+        var height = heightEditText.text.toString().toInt()
+        return length*height
     }
 }
--- app/src/main/java/com/example/areavisualizer/TriangleActivity.kt
+++ app/src/main/java/com/example/areavisualizer/TriangleActivity.kt
@@ -1,11 +1,37 @@
 package com.example.areavisualizer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 
 class TriangleActivity : AppCompatActivity() {
+
+    lateinit var heightEditText : EditText
+    lateinit var baseEditText : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_triangle)
+
+        val triBtn : Button = findViewById(R.id.triangleBtnView)
+
+        heightEditText = findViewById(R.id.triangleHeightEditText)
+        baseEditText = findViewById(R.id.triangleBaseEditText)
+
+        triBtn.setOnClickListener() {
+            calculateArea()
+            val intent = Intent(this, ResultActivity::class.java )
+            intent.putExtra("calculateArea", calculateArea())
+            startActivity(intent)
+        }
+    }
+
+    fun calculateArea() : Int {
+        var length = baseEditText.text.toString().toInt()
+        var height = heightEditText.text.toString().toInt()
+        return (length*height)/2
     }
 }
